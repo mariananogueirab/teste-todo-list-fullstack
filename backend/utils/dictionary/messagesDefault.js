2f065874bc24a332b8bae08b4831fdb938414378
mariananbp <mariananbp@gmail.com>
2022-02-10 20:26:36 -0300

    dictionary

---
+++ backend/utils/dictionary/messagesDefault.js
@@ -0,0 +1,7 @@
+module.exports = {
+  invalidEntry: 'Invalid entries. Try again.',
+  invalidDate: 'Invalid format date. Please enter a date in MM-DD-YYYY format',
+  clientNotFound: 'Client not found',
+  clientAlreadyRegistered: 'This client is already registered',
+  noClientsRegistered: 'There are no clients registered',
+};

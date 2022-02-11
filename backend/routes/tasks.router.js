c3811fb8a0536d5a9582b2c30c9fa68aa4a66181
mariananbp <mariananbp@gmail.com>
2022-02-11 20:31:55 -0300

    router - get by alphabetical order

--- backend/routes/tasks.router.js
+++ backend/routes/tasks.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const {createTask, getTasks} = require('../controllers/tasks.controller');
+const {
+  createTask,
+  getTasks,
+  getTasksByAlphab,
+} = require('../controllers/tasks.controller');
 const auth = require('../middlewares/auth');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/', auth, createTask);
+router.get('/alphabetical', auth, getTasksByAlphab);
 router.get('/', auth, getTasks);
 
 module.exports = router;

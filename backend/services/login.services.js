f5f48642cfe4634a411ccca074069d9c1d4286b0
mariananbp <mariananbp@gmail.com>
2022-02-10 23:12:01 -0300

    services - login

---
+++ backend/services/login.services.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const {findUser} = require('../models/user.model');
+const {userAlreadyRegistered, incorrectData} = require('../utils/dictionary/messagesDefault');
+const {badRequest, conflict, unauthorized} = require('../utils/dictionary/statusCode');
+const errorHandling = require('../utils/functions/errorHandling');
+const {generateToken} = require('./authService');
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+      .email({minDomainSegments: 2, tlds: {allow: ['com', 'net']}}).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const validateLogin = (email, password) => {
+  const {error} = loginSchema.validate({
+    email, password,
+  });
+  if (error) throw errorHandling(badRequest, error.message);
+};
+
+const getUser = async (user) => {
+  const {email, password} = user;
+  const userFound = await findUser(email);
+  if (!userFound || userFound.password !== password) {
+    throw errorHandling(unauthorized, incorrectData);
+  }
+
+  const {password: _password, ...userWithoutPassword} = userFound;
+
+  const token = generateToken(userWithoutPassword);
+  return token;
+};
+
+module.exports = {
+  getUser,
+};
--- backend/services/user.services.js
+++ backend/services/user.services.js
@@ -5,29 +5,29 @@ const {badRequest, conflict} = require('../utils/dictionary/statusCode');
 const errorHandling = require('../utils/functions/errorHandling');
 const {generateToken} = require('./authService');
 
-const userSchema = Joi.object({
+const createUserSchema = Joi.object({
   username: Joi.string().min(5).required(),
   email: Joi.string()
       .email({minDomainSegments: 2, tlds: {allow: ['com', 'net']}}).required(),
   password: Joi.string().min(6).required(),
 });
 
-const validateUser = (username, email, password) => {
-  const {error} = userSchema.validate({
+const validateCreateUser = (username, email, password) => {
+  const {error} = createUserSchema.validate({
     username, email, password,
   });
   if (error) throw errorHandling(badRequest, error.message);
 };
 
-const validateUserAlreadyExists = async (user) => {
-  const userFound = await findUser(user);
+const validateUserAlreadyExists = async (email) => {
+  const userFound = await findUser(email);
   if (userFound) throw errorHandling(conflict, userAlreadyRegistered);
 };
 
 const userCreate = async (user) => {
   const {username, email, password} = user;
-  validateUser(username, email, password);
-  await validateUserAlreadyExists(user);
+  validateCreateUser(username, email, password);
+  await validateUserAlreadyExists(email);
   await create(user);
 
   const {password: _password, ...userWithoutPassword} = user;

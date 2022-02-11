0213aae3e9620427eaa46c833f16482f3f628d50
mariananbp <mariananbp@gmail.com>
2022-02-11 15:31:34 -0300

    router - create task

--- backend/routes/tasks.router.js
+++ backend/routes/tasks.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {createTask} = require('../controllers/tasks.controller');
+const auth = require('../middlewares/auth');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.post('/', createTask);
+router.post('/', auth, createTask);
 
 module.exports = router;

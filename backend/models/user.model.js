af12932b35832ca2b2946dc167caf1ee47b61e20
mariananbp <mariananbp@gmail.com>
2022-02-10 21:09:48 -0300

    model - create user

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+const {ObjectId} = require('mongodb');
+const connect = require('./connection');
+
+const DB_COLLECTION = 'Users';
+
+const create = async (user) => {
+  const {username, email, password} = user;
+  const db = await connect();
+  const {insertedId} = await db.collection(DB_COLLECTION)
+      .insertOne({username, email, password});
+  return insertedId;
+};
+
+const findUser = async (user) => {
+  const db = await connect();
+  const {username, email, password} = user;
+  const userFound = await db.collection(DB_COLLECTION)
+      .find({username, email, password});
+  return userFound;
+};
+
+module.exports = {
+  create,
+  findUser,
+};

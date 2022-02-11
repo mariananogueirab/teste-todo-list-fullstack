0f29117b3421d8e76a151390687654b3fad15ae3
mariananbp <mariananbp@gmail.com>
2022-02-11 15:51:24 -0300

    add user in create task

--- backend/controllers/tasks.controller.js
+++ backend/controllers/tasks.controller.js
@@ -2,14 +2,15 @@ const {taskCreate} = require('../services/tasks.services');
 const {created} = require('../utils/dictionary/statusCode');
 
 const createTask = async (req, res, next) => {
+  const {user} = req;
   try {
-    const id = await taskCreate(req.body);
     const newTask = {
-      _id: id,
       ...req.body,
+      user: user.email,
     };
+    const id = await taskCreate(newTask);
 
-    return res.status(created).json(newTask);
+    return res.status(created).json({_id: id, ...newTask});
   } catch (error) {
     next(error);
   }
--- backend/models/tasks.model.js
+++ backend/models/tasks.model.js
@@ -4,14 +4,16 @@ const DB_COLLECTION = 'Tasks';
 
 const create = async (newTask) => {
   const createdDate = new Date();
-  const {task, limitDate} = newTask;
+  const {task, limitDate, user} = newTask;
   const db = await connect();
   const {insertedId} = await db.collection(DB_COLLECTION)
       .insertOne({
-        task, limitDate, createdDate, completed: 'false',
+        task, limitDate, createdDate, completed: 'false', user,
       });
   return insertedId;
 };
+
+
 
 module.exports = {
   create,

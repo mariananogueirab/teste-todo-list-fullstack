8405bff31900b560742edd242cd021558f5a74d7
mariananbp <mariananbp@gmail.com>
2022-02-10 21:11:29 -0300

    dictionary

--- backend/utils/dictionary/messagesDefault.js
+++ backend/utils/dictionary/messagesDefault.js
@@ -1,7 +1,7 @@
 module.exports = {
   invalidEntry: 'Invalid entries. Try again.',
-  invalidDate: 'Invalid format date. Please enter a date in MM-DD-YYYY format',
-  clientNotFound: 'Client not found',
-  clientAlreadyRegistered: 'This client is already registered',
-  noClientsRegistered: 'There are no clients registered',
+  userAlreadyRegistered: 'User already registered',
+  jwtMalformed: 'Expired or invalid token',
+  userNotFound: 'User does not exist',
+  missingAuth: 'Token not found',
 };

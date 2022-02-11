c8890eb17b2a9578e7ec3e4871dd27398723e3c0
mariananbp <mariananbp@gmail.com>
2022-02-11 20:31:16 -0300

    service - get by alphabetical order

--- backend/services/tasks.services.js
+++ backend/services/tasks.services.js
@@ -1,4 +1,4 @@
-const {create, getAll} = require('../models/tasks.model');
+const {create, getAll, getAllByAlphab} = require('../models/tasks.model');
 const {noTasksYet} = require('../utils/dictionary/messagesDefault');
 const {badRequest, notFound} = require('../utils/dictionary/statusCode');
 const Joi = require('joi').extend(require('@hapi/joi-date'));
@@ -32,7 +32,16 @@ const findTasks = async (user) => {
   return tasks;
 };
 
+const findTasksByAlphab = async (user) => {
+  const tasks = await getAllByAlphab(user);
+
+  if (tasks.length == 0) throw errorHandling(notFound, noTasksYet);
+
+  return tasks;
+};
+
 module.exports = {
   taskCreate,
   findTasks,
+  findTasksByAlphab,
 };

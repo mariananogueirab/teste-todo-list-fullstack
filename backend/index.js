e22ac1f47c72c2bb5e3d975c46658ddda90eef62
mariananbp <mariananbp@gmail.com>
2022-02-10 21:11:15 -0300

    index - create user

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const errorMiddleware = require('./middlewares/errorMidlleware');
+const user = require('./routes/user.router');
 
 const {PORT} = process.env;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/user', user);
 
 app.use(errorMiddleware);
 

8e3ec86f30966e0683ccd04df94ad68d5286084b
mariananbp <mariananbp@gmail.com>
2022-02-11 21:35:23 -0300

    router - get by limit date

--- backend/routes/tasks.router.js
+++ backend/routes/tasks.router.js
@@ -3,6 +3,7 @@ const {
   createTask,
   getTasks,
   getTasksByAlphab,
+  getTasksByLimitDate,
 } = require('../controllers/tasks.controller');
 const auth = require('../middlewares/auth');
 
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.post('/', auth, createTask);
 router.get('/alphabetical', auth, getTasksByAlphab);
+router.get('/limit-date', auth, getTasksByLimitDate);
 router.get('/', auth, getTasks);
 
 module.exports = router;

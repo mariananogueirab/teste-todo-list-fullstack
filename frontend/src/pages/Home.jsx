030cefabc74007c7560b1b7d31f4920beca57e09
mariananbp <mariananbp@gmail.com>
2022-02-13 10:06:45 -0300

    Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../styles/home.css';
+import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
+import logo from '../images/logo.png';
 
 function Home() {
+  const history = useHistory();
+
   return (
-    <div>
-      HOME
+    <div className="div-home">
+      <img src={logo} alt="ideas and commitments" data-testid="logo-photo" />
+      <Button onClick={() => history.push('/get-in')} className="button-get-in" label="GET IN" />
     </div>
   );
 }
+
 export default Home;

364140ffafda00a7430ee5de3b9622111f0183b2
mariananbp <mariananbp@gmail.com>
2022-02-13 10:05:24 -0300

    BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import GetIn from './pages/GetIn';
-import UserProvider from './context/User.Provider';
+import UserProvider from './context/UserProvider';
 import Profile from './pages/Profile';
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <UserProvider>
-      <Routes>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/get-in" component={GetIn} />
         <Route exact path="/profile" component={Profile} />
-      </Routes>
+      </Switch>
     </UserProvider>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
